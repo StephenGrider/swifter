0e0272ef85d72ed61247c75ac18b6800bdb86337
StephenGrider <ste.grider@gmail.com>
2014-12-15 19:09:33 -0800

    Moved API search to a separate class

---
+++ Tables/Tables/APIController.swift
@@ -0,0 +1,43 @@
+//
+//  APIController.swift
+//  Tables
+//
+//  Created by stephen g on 12/15/14.
+//  Copyright (c) 2014 stephen g. All rights reserved.
+//
+
+import Foundation
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController {
+    var delegate: APIControllerProtocol?
+    
+    init() {
+        
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                if(error != nil) { println(error.localizedDescription)}
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) { println("JSON Error \(err!.localizedDescription)") }
+                
+                let results: NSArray = jsonResult["results"] as NSArray
+                self.delegate?.didReceiveAPIResults(jsonResult)
+            })
+            
+            task.resume()
+        }
+    }
+}
--- Tables/Tables/SearchResultsViewController.swift
+++ Tables/Tables/SearchResultsViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
     
     // get a reference to the table view
     @IBOutlet weak var appTableView: UITableView!
     
     var tableData = []
+    var api = APIController()
     
     /*
         Init
@@ -21,8 +22,8 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        searchItunesFor("productivity")
+        api.delegate = self
+        api.searchItunesFor("Pool")
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +34,14 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     /*
         Control
     */
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appTableView!.reloadData()
+        })
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableData.count
@@ -53,30 +62,7 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
         return cell
     }
     
-    func searchItunesFor(searchTerm: String) {
-        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-        
-        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
-            let url = NSURL(string: urlPath)
-            let session = NSURLSession.sharedSession()
-            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
-                if(error != nil) { println(error.localizedDescription)}
-                var err: NSError?
-                
-                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-                if(err != nil) { println("JSON Error \(err!.localizedDescription)") }
-                
-                let results: NSArray = jsonResult["results"] as NSArray
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.tableData = results
-                    self.appTableView!.reloadData()
-                })
-            })
-            
-            task.resume()
-        }
-    }
+    
 }
 
 
