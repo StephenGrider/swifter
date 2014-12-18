59e7d3af135ec265c40a6c2f5cead8d92404303c
StephenGrider <ste.grider@gmail.com>
2014-12-17 18:51:24 -0800

    added nav controller

---
+++ Tables/Tables/DetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewController.swift
+//  Tables
+//
+//  Created by stephen g on 12/17/14.
+//  Copyright (c) 2014 stephen g. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
